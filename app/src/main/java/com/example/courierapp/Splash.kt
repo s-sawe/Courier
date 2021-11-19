a2b74c7375099d0c2762e6e7d2c97c1efd5fbf8c
s-sawe <sawesharonn@gmail.com>
2021-11-19 10:47:57 +0300

    Done with the Splash, Login and Register pages

--- app/src/main/java/com/example/courierapp/Splash.kt
+++ app/src/main/java/com/example/courierapp/Splash.kt
@@ -1,30 +1,32 @@
 package com.example.courierapp
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 
 
 class Splash : Fragment() {
 
+    private lateinit var rootView: View
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_splash, container, false)
-        val ImageView = view.findViewById<TextView>(R.id.next)
-        ImageView.setOnClickListener{
-            findNavController().navigate(R.id.action_splash_to_signUp)
-        }
-        return view
+        splash()
+        rootView = inflater.inflate(R.layout.fragment_splash, container, false)
+        return rootView
     }
 
+    fun splash(){
+        Handler(Looper.getMainLooper()).postDelayed(Runnable { findNavController().navigate(R.id.action_splash_to_signUp)
+        },500)
+
+    }
 }
-
-
